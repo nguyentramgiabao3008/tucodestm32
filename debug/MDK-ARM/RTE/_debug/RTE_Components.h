4f151973041bbf0391a8ba4f56824dfe9ac47c01
Nguyễn Trầm Gia Bảo <https://github.com/nguyentramgiabao3008/Smart-home.git>
2023-03-03 13:20:34 +0700

    nhap nhay 2 led khac tan so

---
+++ debug/MDK-ARM/RTE/_debug/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'debug' 
+ * Target:  'debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
